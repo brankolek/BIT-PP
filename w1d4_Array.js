8a09bf36d09b0889cfda8544632ec0ebac70c24e
BIT student <classroom@bgit.rs>
2018-04-20 14:52:20 +0200

    Array

---
+++ w1d4_Array.js
@@ -0,0 +1,60 @@
+// ***************** ARRAY ***********************
+
+
+// ****************** PRVI ZADATAK ****************
+
+
+
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+// console.log(months[5]);
+// console.log(months[9]);
+// console.log(months[0]);
+
+
+
+// *********************** DRUGI ZADATAK ***********************
+
+
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satruday", "Sunday"];
+
+// console.log(days[6]);
+
+
+
+// ************** TRECI ZADATAK ****************************
+
+
+// var number = [2, -4, 5, -2, -11];
+
+// console.log(number[1]);
+// console.log(number[3]);
+// console.log(number[4]);
+
+
+
+// ******************* CETVRTI ZADATAK ***********************
+
+
+// var num = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+// console.log(num[3]);
+// console.log(num[6]);
+
+
+
+// ******************** PETI ZADATAK **************************
+
+
+
+
+// var num = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+
+// console.log(num[2]);
+// console.log(num[2][1]);
+
+
+
+
+
