008bef452e2f187eaa5ea9807718eb1a7f9d1c92
BIT student <classroom@bgit.rs>
2018-04-25 17:59:08 +0200

    w2d3

---
+++ w2d2_loopsAraays1.js
@@ -0,0 +1,181 @@
+
+
+// ********************************************************** PRVI ZADATAK **********************************************************
+
+// var e = 7;
+// var a = [5, -4.2, 3, 7];
+// var ind = false;
+
+
+// for (i = 0; i < a.length; i++) {
+
+//     if (a[i] === e) {
+
+//         ind = true;
+//     }
+// }
+
+// if (ind === false) {
+
+//     console.log("NO");
+// } else {
+
+
+//     console.log("Yes");
+// }
+
+
+// ******************************************** drugi zad *************************
+/*
+var a = [-3, 11, 5, 3.4, -8];
+ 
+for (i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+        a[i] *= 2;
+    }
+}
+console.log(a);
+ 
+*/
+
+//**********************************treci zad ******************************* */
+/*
+ 
+var a = [-5, -14, 2, -1, -14, -14]
+ 
+var min = a[0];
+var index = 0;
+ 
+ 
+ 
+for (i = 0; i < a.length; i++) {
+ 
+    if (a[i] < min) {
+ 
+        min = a[i];
+        index = i;
+    }
+ 
+}
+ 
+ 
+console.log(min);
+console.log(index);
+*/
+
+
+
+// ***************************** 4 zad ******************************
+
+
+
+// var a = [-15, -15, 2, -7, -1,]
+
+// var min = a[0];
+// var min2 = a[1];
+
+
+// for (i = 1; i < a.length; i++) {
+
+//     if (a[i] < min) {
+
+//         min2 = min;
+//         min = a[i];
+
+//     } else if (a[i] < min2) {
+
+//         min2 = a[i];
+//     }
+
+// }
+
+// console.log(min);
+// console.log(min2);
+
+
+
+
+// ********************************** PETI ZADATAK ********************
+
+
+// var a = [3, 11, -5, -3, 2]
+
+// var sum = 0;
+
+// for (i = 0; i < a.length; i++) {
+
+
+//     if (a[i] > 0) {
+
+//         sum = sum + a[i];
+
+//     }
+
+// }
+
+
+// console.log(sum);
+
+
+
+// ********************************** SESTI ZADATAK ********************
+
+
+
+// var a = [2, -4, 3, 2, 4, 2];
+
+// var ind = true;
+
+// for (i = 0, j = a.length - 1; i <= j; i++ , j--) {
+
+//     if (a[i] !== a[j]) {
+//         ind = false;
+//         break;
+//     }
+
+// }
+
+// if (ind === true) {
+
+//     console.log("simetrican")
+// } else {
+//     console.log("Nije simetrican")
+// }
+
+
+
+// ********************************** SEDMI ZADATAK ********************
+
+
+// var a = [4, 5, 6, 2];
+// var b = [3, 8, 11, 9];
+// var c = [];
+
+
+// for (i = 0, j = 0; i < a.length; i++ , j += 2) {
+
+//     c[j] = a[i];
+//     c[j + 1] = b[i];
+
+// }
+
+// console.log(c)
+
+
+
+// ********************************** OSMI ZADATAK ********************
+
+var a = [4, 5, 6, 2];
+var b = [3, 8, 11, 9]
+var c = [];
+
+for (i = 0; i < a.length; i++) {
+    c[i] = a[i];
+}
+for (i = 0, j = a.length; i < b.length; i++ , j++) {
+    c[j] = b[i];
+}
+
+
+
+console.log(c);
---
+++ w2d3_loopsArrays2.js
@@ -0,0 +1,101 @@
+// ****************************** PETI ZADATAK ************************
+// var i;
+// var j;
+// var n = 19;
+
+// for (i = 0; i < n; i++) {
+//     if (i === 0 || i === n - 1 || i === (n - 1) / 2) {
+//         var stars = "";
+//         for (j = 0; j < n; j++) {
+//             stars = stars + "*"
+//         }
+//         console.log(stars)
+//     } else {
+
+//         var line = '*';
+//         for (j = 1; j <= n - 2; j++) {
+//             if (j === (n - 1) / 2) {
+//                 line += "*"
+//             } else {
+//                 line += " ";
+//             }
+//         }
+//         line += "*";
+//         console.log(line)
+//     }
+// }
+
+
+// ******************* SEDMI ZADATAK ******************
+
+// var n = 527;
+// var digit;
+// var digits = [];
+// var i = 0;
+
+// while (n > 0) {
+
+//     digit = n % 10;
+//     n = (n - digit) / 10;
+
+//     // console.log(digit);
+//     digits[i] = digit;
+//     i++
+
+// }
+// console.log(digits);
+
+
+
+// DOMACI 
+
+// var a = "branko";
+// var b = 9;
+
+// a = [a, b];
+// b = a[0];
+// a = a[1];
+// console.log(a, b);
+
+
+
+// WHILE LOOP
+
+
+// ********************************* DEVETI ZADATAK **************
+
+
+
+// var a = [4, 9, 12, 6, 5, 7, 8, 21, 5];
+// sum = 0;
+
+// var i = 0;
+
+// while (i < a.length) {
+
+//     if (a[i] % 2 !== 0) {
+
+//         sum = sum + a[i];
+
+//     }
+//     i++
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
